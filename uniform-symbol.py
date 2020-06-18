8cea36c6e7c17679e98fbec7cd212de5e08c75e6
Your Name <you@example.com>
2020-06-18 15:54:04 +0800

    data & script

---
+++ uniform-symbol.py
@@ -0,0 +1,27 @@
+# uniform gene symbols
+HGNC_NOW=[]
+HGNC_BF=[]
+
+with open(r'data/hgnc-biomart.txt','r') as f:
+    for line in f:
+        line=line.strip().split('\t')
+        try:
+            HGNC_BF.append(line[4])
+            HGNC_NOW.append(line[2])
+        except:
+            pass
+
+
+def uniform_gene_symbol(gs):
+    gs=str(gs)
+    if gs in HGNC_NOW:
+        return gs
+    elif gs in HGNC_BF:
+        return HGNC_NOW[HGNC_BF.index(i)]
+    else:
+        print("the gene symbol is not exist!")
+
+
+if __name__=='__main__':
+    pass
+
